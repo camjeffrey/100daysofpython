79e8e3274880a0dc90cfa12b5e09974d6c79b371
Campbell Milne <camjeffrey97@gmail.com>
2025-03-19 21:28:45 +1100

    Added new line characters at end of input fields, added welcome message.

--- day_1/band_name_generator.py
+++ day_1/band_name_generator.py
@@ -1,4 +1,6 @@
-city = input("Which city did you grow up in?: ")
-pet =  input("What was the name of your first pet?: ")
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow up in?: \n")
+pet =  input("What was the name of your first pet?: \n")
 
 print(f"Your band name could be '{city} {pet}.'")
