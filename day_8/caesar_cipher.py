fdd8b78ba9691af304ef783662c729324267801d
camjeffrey <camjeffrey97@gmail.com>
2025-03-27 00:28:55 +1100

    Removed commented code.

--- day_8/caesar_cipher.py
+++ day_8/caesar_cipher.py
@@ -3,42 +3,6 @@ import string
 lowercase_alphabet = list(string.ascii_lowercase)
 uppercase_alphabet = list(string.ascii_uppercase)
 
-# def encryption(message, shift):
-#     unencrypted_message = list(message)
-#     encrypted_message = ""
-# 
-#     for i in unencrypted_message:
-#         if i == ' ' or i in string.punctuation or i in string.digits:
-#             encrypted_message += i
-#         elif i in lowercase_alphabet_list:
-#             unencrypted_index = lowercase_alphabet_list.index(i)
-#             encrypted_index = unencrypted_index + shift
-#             encrypted_message += lowercase_alphabet_list[encrypted_index % len(lowercase_alphabet_list)]
-#         elif i in uppercase_alphabet_list:
-#             unencrypted_index = uppercase_alphabet_list.index(i)
-#             encrypted_index = unencrypted_index + shift
-#             encrypted_message += uppercase_alphabet_list[encrypted_index % len(uppercase_alphabet_list)]
-# 
-#     return encrypted_message
-# 
-# def decryption(message, shift):
-#     encrypted_message = list(message)
-#     unencrypted_message = ""
-# 
-#     for i in encrypted_message:
-#         if i == ' ' or i in string.punctuation or i in string.digits:
-#             unencrypted_message += i
-#         elif i in lowercase_alphabet_list:
-#             encrypted_index = lowercase_alphabet_list.index(i)
-#             unencrypted_index = encrypted_index - shift
-#             unencrypted_message += lowercase_alphabet_list[unencrypted_index % len(lowercase_alphabet_list)]
-#         elif i in uppercase_alphabet_list:
-#             encrypted_index = uppercase_alphabet_list.index(i)
-#             unencrypted_index = encrypted_index - shift
-#             unencrypted_message += uppercase_alphabet_list[unencrypted_index % len(uppercase_alphabet_list)]
-# 
-#     return unencrypted_message
-
 def caesar_cipher(text, shift, encrypt = True):
     cipher = ""
     message = list(text)
